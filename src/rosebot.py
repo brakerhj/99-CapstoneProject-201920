8d639d98fd16bf57177281f11d49b2581e9306a0
brammetc <brammetc@rose-hulman.edu>
2019-02-06 13:06:35 -0500

    lower arm function

--- src/rosebot.py
+++ src/rosebot.py
@@ -231,7 +231,7 @@ class ArmAndClaw(object):
         self.motor.reset_position()
         self.motor.turn_on(-100)
         while True:
-            if abs(self.motor.get_position()) is 14.2 * 360:
+            if abs(self.motor.get_position()) >= 14.2 * 360:
                 self.motor.turn_off()
                 break
 ###############################################################################
