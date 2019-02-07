aa4b2a866d81b853127e076fc0b4c1e957775f43
brammetc <brammetc@rose-hulman.edu>
2019-02-07 12:17:37 -0500

    Stuff

--- src/m2_run_this_on_robot.py
+++ src/m2_run_this_on_robot.py
@@ -8,6 +8,7 @@
 import rosebot
 import mqtt_remote_method_calls as com
 import time
+import shared_gui_delegate_on_robot
 
 
 def main():
@@ -16,7 +17,16 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
+    real_thing()
 
+
+def real_thing():
+    robot = rosebot.RoseBot()
+    delegate_receives = shared_gui_delegate_on_robot.DelegateReceives(robot)
+    mqtt_receivers = com.MqttClient(delegate_receives)
+    mqtt_receivers.connect_to_pc()
+    while True:
+        time.sleep(0.01)
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
