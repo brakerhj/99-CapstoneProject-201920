f85fa9c74b835fb0cbb90c3c6cdcc8e422e0dede
brammetc <brammetc@rose-hulman.edu>
2019-02-10 19:06:32 -0500

    raise arm funct

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -219,13 +219,14 @@ def handle_raise_arm(mqtt_sender):
       :type  mqtt_sender:  com.MqttClient
     """
     print('raise arm')
-    mqtt_sender.send_message()
+    mqtt_sender.send_message('raise_arm')
 
 def handle_lower_arm(mqtt_sender):
     """
     Tells the robot to lower its Arm until it is all the way down.
       :type  mqtt_sender:  com.MqttClient
     """
+    
 
 
 def handle_calibrate_arm(mqtt_sender):
