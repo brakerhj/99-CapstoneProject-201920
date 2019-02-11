418db8238a28500de6866f7f2783ea1ad9991fa4
brammetc <brammetc@rose-hulman.edu>
2019-02-10 21:08:45 -0500

    Gui update

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -302,12 +302,12 @@ def handle_exit(mqtt_sender):
 
 def handle_go_straight_seconds(go_straight_seconds_seconds, mqtt_sender):
     "sets up the handle."
-    print('going straight for', go_straight_seconds_seconds)
+    print('going straight for', int(go_straight_seconds_seconds.get()))
     mqtt_sender.send_message('go_straight_for_seconds', [int(go_straight_seconds_seconds.get())])
 
 def handle_go_straight_inches(go_straight_inches_speed, go_straight_inches_inches, mqtt_sender):
     "sets up the handle."
     print('going straight for inches')
-    mqtt_sender.send_message('go_straight_for_inches', [go_straight_inches_speed.get(), go_straight_inches_inches.get()])
+    mqtt_sender.send_message('go_straight_for_inches', [int(go_straight_inches_speed.get()), int(go_straight_inches_inches.get())])
 def handle_go_straight_inches_encoder(mqtt_sender):
     "sets up the handle."
